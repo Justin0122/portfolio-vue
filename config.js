9efdcca2589d617d1e52e3d2f01a3df3f7ff4711
Justin Jongstra <Justinjongstra8@gmail.com>
2024-01-10 11:54:12 +0100

    .

--- config.js
+++ config.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 export const BASE_URL = 'https://api.justinjongstra.nl';
-=======
-export const BASE_URL = 'http://localhost:3002';
 export const FULL_NAME = 'Justin Jongstra';
 export const PROFILE_PIC_URL = 'https://avatars.githubusercontent.com/u/85618156?v=4';
->>>>>>> 50cb51b303c8d68f03cb2e674fc41970cf5f796d
