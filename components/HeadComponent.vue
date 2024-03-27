bd7d040cc9309725bf2c6bdcc721888fc94dfc0f
Justin Jongstra <00300061@rijnijssel.nl>
2024-03-27 21:40:30 +0100

    added meta_pic_url in config

--- components/HeadComponent.vue
+++ components/HeadComponent.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
-import { FULL_NAME, PROFILE_PIC_URL } from '~/config.js';
+import { FULL_NAME, PROFILE_PIC_URL, META_PIC_URL } from '~/config.js';
 const name = FULL_NAME;
 const profilePicUrl = PROFILE_PIC_URL;
+const metaPicUrl = META_PIC_URL;
 
 useHead({
   title: name + ' - Portfolio',
@@ -9,7 +10,7 @@ useHead({
     { name: 'description', content: `Hi! I'm ${name}, a full stack developer from the Netherlands. Check out my portfolio!` },
     { name: 'og:title', content: `${name} - Portfolio` },
     { name: 'og:description', content: `Hi! I'm ${name}, a full stack developer from the Netherlands. Check out my portfolio!` },
-    { name: 'og:image', content: `${profilePicUrl}` },
+    { name: 'og:image', content: `${metaPicUrl}` },
     { name: 'og:image:alt', content: `${name} - Portfolio` },
     { name: 'og:url', content: `https://${name.split(' ').join('').toLowerCase()}.nl` },
     { name: 'og:site_name', content: `${name} - Portfolio` },
@@ -18,7 +19,7 @@ useHead({
     { name: 'twitter:creator', content: '@justinjongstra' },
     { name: 'twitter:title', content: `${name} - Portfolio` },
     { name: 'twitter:description', content: `Hi! I'm ${name}, a full stack developer from the Netherlands. Check out my portfolio!` },
-    { name: 'twitter:image', content: `${profilePicUrl}` },
+    { name: 'twitter:image', content: `${metaPicUrl}` },
     { name: 'twitter:image:alt', content: `${name} - Portfolio` }
   ],
   link: [
--- config.example.js
+++ config.example.js
@@ -1,3 +1,4 @@
 export const BASE_URL = 'http://localhost:3002';
 export const FULL_NAME = 'Your Name';
 export const PROFILE_PIC_URL = 'your profile pic url';
+export const META_PIC_URL = 'your meta pic url';
